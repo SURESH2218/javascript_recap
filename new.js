83e565fd73639cc0ac00356dd476ed8de308d235
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-07-12 13:41:34 +0530

    first class functions

--- new.js
+++ new.js
@@ -19,5 +19,39 @@
 // abcd1();
 
 // in javascript we actually use some features which are not there in the language but we use
-// thoe features through window 
+// thoe features through window
 // window ------->>>> box of features given by browser to use with js
+
+//setTimeout function
+
+// setTimeout(function () {
+//   console.log("after 5 seconds");
+// }, 5000);
+
+//first class functions
+
+// if we use fucntion as a value then we call it as a first class function
+// we can save everything below here in a variable
+
+// var a = 23;
+// var a = true;
+// var a = function() {
+//   console.log("suresh alabani");
+// };
+// a();
+
+// function abcd(a) {
+//   a();
+// }
+// abcd(function () {
+//   console.log("suresh");
+// });
+
+//array or object
+
+var a = {
+  age: 0,
+  name: "suresh",
+};
+var b = [1, 3, 3, 5, 6, 7];
+console.log(typeof a, Array.isArray(a), Array.isArray(b));
